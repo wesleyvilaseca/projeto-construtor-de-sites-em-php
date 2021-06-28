108567cf435c3cf698fb53c43f9d08a0b2f5ce62
Wesley Vila Seca Sanches <wesley.vilaseca@gmail.com>
2021-06-28 01:25:40 -0300

    # adjust # core reorganization

--- app/core/Core.php
+++ app/core/Core.php
@@ -1,6 +1,10 @@
 <?php
 
-use app\core\Controller;
+/**
+ * a Doc é meio capitão óbvio (para não leigos), mas pode ser que ajude
+ * 
+ * by Wesley Vila Seca
+ */
 
 class Core
 {
@@ -16,71 +20,100 @@ class Core
 
     public function run()
     {
-        $controllerCorrente = $this->getController();
-
-        $c = new $controllerCorrente();
-        //no call user eu passo um array, onde primeiro vai o objeto, em seguida eu passo o método
-        //e após isso eu passo os parêmetros que o método em questão vai receber
-        call_user_func_array(array($c, $this->getMetodo()), $this->getParametros());
+        /**
+         * no call user eu passo um array, onde primeiro vai o objeto, em seguida eu passo o método
+         * e após isso eu passo os parêmetros que o método em questão vai receber
+         * 
+         */
+        $currentController  = $this->getController();
+        $c                  = new $currentController();
+        call_user_func_array(array($c, $this->getMethod()), $this->getParam());
     }
 
     public function verificaUri()
     {
-        //o metodo run vai ser o responsável por administrar o controller que vai ser executado
-        //local
-        //$url = explode("index.php", $_SERVER["PHP_SELF"]);
-
-        //onServer
-        $url = explode("index.php", $_SERVER["PHP_SELF"]);
-        $url = end($url);
+        /**
+         * o metodo run vai ser o responsável por administrar o controller que vai ser executado
+         * 
+         * $url = explode("index.php", $_SERVER["PHP_SELF"]); -> local
+         * $url = explode("index.php", $_SERVER["REQUEST_URI"]); -> local
+         * 
+         * Depois de verificado, se o site se encontra em manutenção, é feito um tratamento na string capturada
+         * com ela extraio a string apos a urlbase, para identificar o controller
+         * em seguida extrai o metodo dentro do controller 
+         * e depois extrai os parametros
+         */
 
         if (EM_MANUTENCAO) {
             $this->controller = 'MaintenanceController';
         } else {
+            $url = explode("index.php", $_SERVER["PHP_SELF"]);
+            $url = end($url);
             if ($url != "") {
                 $url = explode('/', $url);
-                //pega o controller
                 array_shift($url);
-                if (strpos($url[0], '-') == true) { //verifica se o controller está em algum diretório dentro da pasta controllers
-                    $route = explode('-', $url[0]);
-                    for ($i = 0; $i < sizeof($route); $i++) {
-                        ($i < (sizeof($route) - 1)) ? $this->route .= $route[$i] . '\\' : $this->controller = $this->route . ucfirst($route[$i]) . "Controller";
-                    }
-                } else {
-                    $controller = ucfirst($url[0]);
-                    if ($controller == 'Page') { //for pages default
-                        $controller = 'institucional\\common\\Page';
-                        $this->controller = $controller . "Controller";
-                    } else {
-                        $this->controller = $controller . "Controller";
-                    }
-                }
-                //pega o metodo
-                array_shift($url);
-                if (isset($url[0])) {
-                    $this->metodo = $url[0];
-                    //pega os parametros
-                    array_shift($url);
-                }
+                $this->setController($url);
 
-                if (isset($url[0])) {
-                    //pega os parametros
-                    $this->parametros = array_filter($url);
-                }
+                array_shift($url);
+                if (isset($url[0])) $this->setMethod($url);
+
+                if (isset($url[0])) $this->setParam($url);
+
             } else {
-                if (strpos(CONTROLLER_PADRAO, '\\') == true) {
-                    $route = explode('\\', CONTROLLER_PADRAO);
-                    for ($i = 0; $i < sizeof($route); $i++) {
-                        ($i < (sizeof($route) - 1)) ? $this->route .= $route[$i] . '\\' : $this->controller = $this->route . ucfirst($route[$i]) . "Controller";
-                    }
-                } else {
-                    $this->controller = ucfirst(CONTROLLER_PADRAO) . "Controller";
-                }
+                $this->setDefaultController();
             }
         }
     }
 
-    function getController()
+    private function setController($url)
+    {
+        /**
+         * com a url em mãos, é verificado, se o controller se encontra em algum diretório. O caminho para o diretorio, é feito com os hifens '-'
+         * 
+         * caso não tenha hifen, é vericado, se as string capturada, é algum controller pré definido, se não, segue o fluxo
+         */
+        if (strpos($url[0], '-') == true) {
+            $route = explode('-', $url[0]);
+            for ($i = 0; $i < sizeof($route); $i++) {
+                ($i < (sizeof($route) - 1)) ? $this->route .= $route[$i] . '\\' : $this->controller = $this->route . ucfirst($route[$i]) . "Controller";
+            }
+        } else {
+            $controller = ucfirst($url[0]);
+            switch ($controller) {
+                case "Page":
+                    $controller = 'institucional\\common\\Page';
+                    $this->controller = $controller . "Controller";
+                    break;
+                default:
+                    $this->controller = $controller . "Controller";
+                    break;
+            }
+        }
+    }
+
+    private function setDefaultController()
+    {
+        if (strpos(CONTROLLER_PADRAO, '\\') == true) {
+            $route = explode('\\', CONTROLLER_PADRAO);
+            for ($i = 0; $i < sizeof($route); $i++) {
+                ($i < (sizeof($route) - 1)) ? $this->route .= $route[$i] . '\\' : $this->controller = $this->route . ucfirst($route[$i]) . "Controller";
+            }
+        } else {
+            $this->controller = ucfirst(CONTROLLER_PADRAO) . "Controller";
+        }
+    }
+
+    private function setMethod($url)
+    {
+        $this->metodo = $url[0];
+        array_shift($url); 
+    }
+
+    private function setParam($url){
+        $this->parametros = array_filter($url); 
+    }
+
+    private function getController()
     {
         if (class_exists(NAMESPACE_CONTROLLER . $this->controller)) {
             return NAMESPACE_CONTROLLER . $this->controller;
@@ -88,9 +121,9 @@ class Core
         return NAMESPACE_CONTROLLER . ucfirst(CONTROLLER_PADRAO) . "Controller";
     }
 
-    function getMetodo()
+    private function getMethod()
     {
-        //verifica se exeiste método dentro da classe do objeto
+        //verifica se existe metodo dentro da classe do objeto
         if (method_exists(Core::getController(), $this->metodo)) {
             return $this->metodo;
         }
@@ -98,7 +131,7 @@ class Core
         return METODO_PADRAO;
     }
 
-    function getParametros()
+    private function getParam()
     {
         return $this->parametros;
     }
